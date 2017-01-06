7a70bac0ee19e5f2c3e07b0686961e1013f5a206
yanghaa <yanghaa@sina.com>
2017-01-07 05:32:38 +0800

    add :  FashetxLocator and fix : zcml class

--- emc/kb/interfaces.py
+++ emc/kb/interfaces.py
@@ -3,7 +3,7 @@ from zope.interface import Interface
 from zope import schema
 
 from emc.kb import _
-    
+
 class InputError(Exception):
     """Exception raised if there is an error making a data input
     """
@@ -11,65 +11,72 @@ class InputError(Exception):
 # db insterface
 class IModelLocator (Interface):
     """medel table add row"""
-    
+
     def addModel(self):
         "add a model data"
-        
+
     def queryModel(self):
         "query model by search condition"
-        
+
 class IBranchLocator (Interface):
     """medel table add row"""
-    
+
     def addBranch(self):
         "add a model data"
-        
+
     def queryBranch(self):
         "query model by search condition"
 
 class IFashejLocator (Interface):
     """fashej table add row"""
-    
+
     def add(**kwargs):
         "add a row data"
-        
+
     def query(code):
         "query  by search condition"
-        
+
     def deleteByCode(code):
         "query  by search condition"
-        
-    def updateByCode(code):
-        "query  by search condition"                
-        
-class IFashetxLocator (IFashejLocator):
-    """fashetx table add row"""  
 
-        
+    def updateByCode(code):
+        "query  by search condition"
+
+class IFashetxLocator (IFashejLocator):
+    """fashetx table add row"""
+
+    def add(**kwargs):
+        "add a row data"
+
+    def query(code):
+        "query  by search condition"
+
+    def deleteByCode(code):
+        "query  by search condition"
+
+    def updateByCode(code):
+        "query  by search condition"
+
 class IJieshoujLocator (IFashejLocator):
     """jieshouj table add row"""
-    
+
 class IJieshoutxLocator (IFashejLocator):
     """jieshouj table add row"""
-    
+
 class ILvboqLocator (IFashejLocator):
     """jieshouj table add row"""
-    
+
 class IJieshoujLocator (IFashejLocator):
     """jieshouj table add row"""
-    
+
 class IDianxingtxzyzkLocator (IFashejLocator):
-    """jieshouj table add row""" 
-    
+    """jieshouj table add row"""
+
 class ITianxianzyzkLocator (IFashejLocator):
-    """jieshouj table add row""" 
-    
+    """jieshouj table add row"""
+
 class IJieshoujzkLocator (IFashejLocator):
-    """jieshouj table add row""" 
-    
+    """jieshouj table add row"""
+
 class IFashejzkLocator (IFashejLocator):
-    """jieshouj table add row""" 
-    
-                               
-                           
-    
+    """jieshouj table add row"""
