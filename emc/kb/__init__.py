36e42418a3777919372626eb80fbd6756424ff11
adam139 <yuejun.tang@gmail.com>
2018-10-27 22:02:43 +0800

    add oracle session

--- emc/kb/__init__.py
+++ emc/kb/__init__.py
@@ -14,13 +14,13 @@ InputDb = "emc.kb:Input db"
 DoVote = "emc.kb:Do vote"
 
 ORMBase = declarative.declarative_base()
-
+ora_engine = create_engine('oracle://sys:password@kwsensen.f3322.net:1521/orcl2?mode=2', pool_recycle=3600)
 some_engine = create_engine('mysql://kbdba:K0mdba$!9@127.0.0.1:3306/parameters?charset=utf8', pool_recycle=3600)
 emc_engine = create_engine('mysql://kbdba:K0mdba$!9@127.0.0.1:3306/emc_test?charset=utf8', pool_recycle=3600)
 log_engine = create_engine('mysql://kbdba:K0mdba$!9@127.0.0.1:3306/emclog?charset=utf8', pool_recycle=3600)
 Session = sessionmaker(bind=some_engine)
 Session_emc = sessionmaker(bind=emc_engine)
-Session_log = sessionmaker(bind=log_engine)
+Session_log = sessionmaker(bind=ora_engine)
 kb_session = Session()
 t_session = Session_emc()
 log_session = Session_log()
